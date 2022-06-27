4e32a47182e310897095a54117d343a884a29819
hlbv17 <helenlbv17@gmail.com>
2022-06-27 00:02:38 -0500

    .

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,14 +1,20 @@
+import { AgregarUserComponent } from './user/agregar-user/agregar-user.component';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { HomeComponent } from "./home/home.component";
 import { HeaderComponent } from "./header/header.component";
+import { ListarCatalogoComponent } from './catalogo/listar-catalogo/listar-catalogo.component';
+import { ListarUserComponent } from './user/listar-user/listar-user.component';
 
 //route
 const routes:Routes=[
     {path:'', component:HomeComponent},
     {path:'login',component:LoginComponent},
-    {path:'home',component:HomeComponent}
+    {path:'home',component:HomeComponent},
+    {path:'agregar-user',component:AgregarUserComponent},
+    {path:'listar-user',component:ListarUserComponent},
+    {path:'listar-catalogo',component:ListarCatalogoComponent},
 ]
 
 @NgModule({
