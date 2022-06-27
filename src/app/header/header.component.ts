603a6494ec59370683649ee630250e3616f989fe
nefi.reyest@ug.edu.ec <WxYhvDn4XYQ5fsL>
2022-06-27 13:01:27 -0500

    Featers header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import {Router} from '@angular/router';
+import { WindowModelService } from 'app/services/window-model.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent  {
+export class HeaderComponent implements OnInit {
 
-  constructor(private dialog:MatDialog, private router:Router) { }
-
+  isLogged!: boolean;
+  constructor(private service: WindowModelService,private dialog:MatDialog, private router:Router) {
+    this.isLogged = false;
+    this.service.$modal.subscribe(valor => {
+      this.isLogged = valor
+      console.log("valor: "+valor);
+    })
+   }
 
   sendLogin(): void{
     if (this.router != null){
         this.router.navigate(['login']);
     }
+  }
+
+  ngOnInit(): void {
+  }
+
+  cerrarSesion(){
+    this.router.navigate(['/home']);
+    this.service.$modal.emit(false);
   }
   
 }
